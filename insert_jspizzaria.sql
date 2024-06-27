125e3357db210afa22aeeab3903edb0a2c9388d9
CAUA SILVA CHAGAS <caua.schagas@senacsp.edu.br>
2024-06-26 22:25:37 -0300

    Inserindo usuarios

---
+++ insert_jspizzaria.sql
@@ -0,0 +1,11 @@
+INSERT INTO perfil (nomePerfil)
+VALUES ('Adm'),
+		 ('Recepcionista'),
+		 ('Cliente');
+		 
+SELECT * FROM usuario
+
+INSERT INTO usuario (idPerfil, nomeUsuario, cpfUsuario, login, senha)
+VALUES (34, 'Juca Simões', '11122233344', 'juca.simoes@jspizzaria.com', 'adm12345'),
+	    (35, 'Rebacca Santos', '99988877766', 'rebecca.santos@jspizzaria.com', 'rec12345');
+
